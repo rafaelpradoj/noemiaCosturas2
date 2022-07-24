37d89690417710c93f60473e9a8c3478f7afe8cc
Rafael Prado de Jesus <rafaelpradoj@gmail.com>
2022-07-24 16:20:02 -0300

    Finalizado primeira versão do projeto

---
+++ assets/js/form.js
@@ -0,0 +1,63 @@
+const form = document.querySelector("form");
+const button = document.querySelector(".button-submit");
+const phoneInput = document.querySelector("#phone-input");
+
+const disableButton = () => {
+  button.setAttribute("disabled", true)
+
+  setTimeout(() => {
+    button.removeAttribute("disabled")
+  }, 60000)
+}
+const submitSuccessMessage = () => {
+  Swal.fire({
+    icon: "success",
+    title: "Tudo ok!",
+    text: "Dados enviados com sucesso!"
+  })  
+}
+const submitErrorMessage = () => {
+  Swal.fire({
+    icon: "error",
+    title: "Oops... Algo deu errado!",
+    text: "Verifique seus dados e tente novamente."
+  })
+}
+const emailJSInit = () => {
+  emailjs.init('user_IFgyDb7wGDHBg6AFzO23k')
+}
+const sendFormToEmail = () => {
+  emailjs.sendForm('service_hm6yc5r', 'template_bfz83kz', form)
+    .then(submitSuccessMessage, submitErrorMessage)
+}
+
+const createPhoneMask = () => {
+  let phone = phoneInput.value.replace(/\D/g, "")
+
+  const phoneFieldLength = phone.length
+  const fieldLengthBiggerThan10 = phone.replace(/^(\d\d)(\d{5})(\d{4}).*/,"($1) $2-$3")
+  const fieldLengthBiggerThan5 = phone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/,"($1) $2-$3")
+  const fieldLengthBiggerThan2 = phone.replace(/^(\d\d)(\d{0,5})/, "($1) $2")
+
+  phone = phone.replace(/^0/, "")
+
+  if (phoneFieldLength > 10) {
+    phone = fieldLengthBiggerThan10
+  } else if (phoneFieldLength > 5) {
+    phone = fieldLengthBiggerThan5
+  } else if (phoneFieldLength > 2) {
+    phone = fieldLengthBiggerThan2
+  }
+
+  phoneInput.value = phone
+}
+const validateForm = event => {
+  event.preventDefault()
+  disableButton()
+  sendFormToEmail()
+}
+
+emailJSInit()
+
+phoneInput.addEventListener("input", createPhoneMask)
+form.addEventListener('submit', validateForm)
